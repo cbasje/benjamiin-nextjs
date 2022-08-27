9d53b1716b8784a186c0d9a006efe226611d639a
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-08-27 17:13:19 +0200

    Update IFrameBlock.tsx

--- components/blocks/IFrameBlock.tsx
+++ components/blocks/IFrameBlock.tsx
@@ -2,10 +2,20 @@ import { styled } from "@/stitches.config";
 
 import { IframeBlock as IframeBlockType } from "@/models/block";
 
-const Iframe = styled("iframe", {});
+const Iframe = styled("iframe", {
+    aspectRatio: "16 / 9",
+    width: "100%",
+    borderRadius: "$sm",
+});
 
 const IframeBlock = ({ url }: IframeBlockType) => {
-    return <Iframe src={url} />;
+    return (
+        <Iframe
+            src={url}
+            loading="lazy"
+            allow="layout-animations 'none'; fullscreen; geolocation 'none'; camera 'none'; microphone 'none'"
+        />
+    );
 };
 
 export default IframeBlock;
